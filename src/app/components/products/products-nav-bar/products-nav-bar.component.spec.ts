8ab69f3bf975095a6321aba41b61e87bb03f528e
Jamal eddine Reda <jamal@macbook-pro-de-jamal.home>
2021-03-18 22:21:06 +0100

    update dependencies

--- src/app/components/products/products-nav-bar/products-nav-bar.component.spec.ts
+++ src/app/components/products/products-nav-bar/products-nav-bar.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ProductsNavBarComponent } from './products-nav-bar.component';
 
@@ -6,7 +6,7 @@ describe('ProductsNavBarComponent', () => {
   let component: ProductsNavBarComponent;
   let fixture: ComponentFixture<ProductsNavBarComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ProductsNavBarComponent ]
     })
